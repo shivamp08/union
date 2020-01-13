3b1bff863ebe307646b9a03db77d6c10f553ad0e
Shivam Patel <Shivam-patel@live.com>
2020-01-13 12:09:18 -0500

    Javafx Window Initialization

--- src/main/java/venn/Main.java
+++ src/main/java/venn/Main.java
@@ -1,14 +1,24 @@
 package venn;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Venn");
 		
-		System.out.println("Hello world");
-		System.out.println("Hello Universe --Shivam");
-		System.out.println("Hello World2 --Colin");
-		
+		Group root = new Group(); 
+		Scene scene = new Scene(root, 1000, 500);
+		primaryStage.setScene(scene);
+		primaryStage.show(); 
 	}
 
 }
