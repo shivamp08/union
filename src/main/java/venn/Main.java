dff67e735249c3ab602995b8b50b037188b088c2
Colin D'Souza <colindis@my.yorku.ca>
2020-01-13 12:43:27 -0500

    commit

--- src/main/java/venn/Main.java
+++ src/main/java/venn/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Venn!");
+		primaryStage.setTitle("Venn");
 		
 		Group root = new Group(); 
 		Scene scene = new Scene(root, 1000, 500);
