b9a7a3ed0f190342e0a960fac44563a85defafca
Colin D'Souza <colindis@my.yorku.ca>
2020-01-22 07:25:12 -0500

    playing around with dragging and dropping

--- src/main/java/venn/Main.java
+++ src/main/java/venn/Main.java
@@ -3,10 +3,14 @@ package venn;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
 import javafx.stage.Stage;
 import javafx.scene.shape.*;
+import javafx.scene.text.Text;
 import javafx.scene.control.*;
 import javafx.scene.paint.Color;
+import javafx.event.*;
+import javafx.scene.input.*;
 
 public class Main extends Application {
 	
@@ -56,17 +60,84 @@ public class Main extends Application {
 		
 		Shape intersect = Shape.intersect(left, right);
 		
-		Color mix = new Color(
-			0.5 * leftColor.getRed() + 0.5 * rightColor.getRed(),
-			0.5 * leftColor.getGreen() + 0.5 * rightColor.getGreen(),
-			0.5 * leftColor.getBlue() + 0.5 * rightColor.getBlue(),
-			1
-		);
-		
-		intersect.setFill(mix);
+		intersect.setFill(Color.LIGHTGREEN);
 		intersect.setStroke(Color.WHITE);
 		intersect.setStrokeWidth(strokeWidth);
 		
+		intersect.setOnDragEntered(new EventHandler<DragEvent>() {
+		    public void handle(DragEvent event) {
+		    /* the drag-and-drop gesture entered the target */
+		    /* show to the user that it is an actual gesture target */
+		         if (event.getGestureSource() != intersect &&
+		                 event.getDragboard().hasString()) {
+		             intersect.setFill(Color.GREEN);
+		         }
+		                
+		         event.consume();
+		    }
+		});
+		
+		left.setOnDragEntered(new EventHandler<DragEvent>() {
+		    public void handle(DragEvent event) {
+		    /* the drag-and-drop gesture entered the target */
+		    /* show to the user that it is an actual gesture target */
+		         if (event.getGestureSource() != left &&
+		                 event.getDragboard().hasString()) {
+		             left.setFill(Color.DARKBLUE);
+		         }
+		                
+		         event.consume();
+		    }
+		});
+		
+		right.setOnDragEntered(new EventHandler<DragEvent>() {
+		    public void handle(DragEvent event) {
+		    /* the drag-and-drop gesture entered the target */
+		    /* show to the user that it is an actual gesture target */
+		         if (event.getGestureSource() != right &&
+		                 event.getDragboard().hasString()) {
+		             right.setFill(Color.DARKRED);
+		         }
+		                
+		         event.consume();
+		    }
+		});
+		
+		intersect.setOnDragExited(new EventHandler<DragEvent>() {
+		    public void handle(DragEvent event) {
+		        /* mouse moved away, remove the graphical cues */
+		        intersect.setFill(Color.LIGHTGREEN);
+
+		        event.consume();
+		    }
+		});
+		
+		left.setOnDragExited(new EventHandler<DragEvent>() {
+		    public void handle(DragEvent event) {
+		        /* mouse moved away, remove the graphical cues */
+		        left.setFill(Color.BLUE);
+
+		        event.consume();
+		    }
+		});
+
+		right.setOnDragExited(new EventHandler<DragEvent>() {
+		    public void handle(DragEvent event) {
+		        /* mouse moved away, remove the graphical cues */
+		        right.setFill(Color.RED);
+
+		        event.consume();
+		    }
+		});
+		
+		intersect.setOnDragDropped(new EventHandler<DragEvent>() {
+			public void handle(DragEvent event) {
+				event.setDropCompleted(true);
+				
+				event.consume();
+			}
+		});
+
 		this.vennGroup.getChildren().clear();
 		
 		this.vennGroup.getChildren().addAll(left, right, intersect);
@@ -85,6 +156,49 @@ public class Main extends Application {
 		Button button = new Button("Hover Over Me");
 		button.setTooltip(new Tooltip("Tooltip for Button"));
 		
+		button.setOnDragDetected(new EventHandler<MouseEvent>() {
+		    public void handle(MouseEvent event) {
+		        /* drag was detected, start a drag-and-drop gesture*/
+		        /* allow any transfer mode */
+		        Dragboard db = button.startDragAndDrop(TransferMode.ANY);
+		        
+		        Group previewGroup = new Group();
+		        
+		        Circle preview = new Circle();
+		        preview.setRadius(20);
+		        preview.setFill(Color.ORANGERED);
+		        
+		        Text previewText = new Text("1");
+		        previewText.setFill(Color.ANTIQUEWHITE);
+		        
+		        previewText.setTranslateX(2);
+		        previewText.setTranslateY(2);
+		        
+		        previewGroup.getChildren().addAll(preview, previewText);
+		        
+		        SnapshotParameters sp = new SnapshotParameters();
+		        sp.setFill(Color.TRANSPARENT);
+		        
+		        db.setDragView(previewGroup.snapshot(sp, null), event.getX(), event.getY());
+		        
+		        /* Put a string on a dragboard */
+		        ClipboardContent content = new ClipboardContent();
+		        content.putString(button.getText());
+		        db.setContent(content);
+		        
+		        event.consume();
+		    }
+		});
+
+		button.setOnDragDone(new EventHandler<DragEvent>() {
+			public void handle(DragEvent event) {
+//				System.out.println(event.get);
+				System.out.println(button.getLayoutX());
+				
+				event.consume();
+			}
+		});
+
 		this.root.getChildren().add(button);
 		
 		this.scene = new Scene(root, Main.width, Main.height);
