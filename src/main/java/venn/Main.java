0cc1810428fe6396c08e2e62aa0f293f7b1e883a
rushabh-chauhan <rushabhh468@sgvp.in>
2020-02-06 17:52:56 -0500

    new

--- src/main/java/venn/Main.java
+++ src/main/java/venn/Main.java
@@ -13,6 +13,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -45,10 +48,75 @@ public class Main extends Application {
 	    vbox.setPadding(new Insets(10));
 		
 
-		//List of data
+		//trial
+	    
+//	    button.setOnDragDetected(new EventHandler<MouseEvent>() {
+//		    public void handle(MouseEvent event) {
+//		        /* drag was detected, start a drag-and-drop gesture*/
+//		        /* allow any transfer mode */
+//		        Dragboard db = button.startDragAndDrop(TransferMode.ANY);
+//		        
+//		        Group previewGroup = new Group();
+//		        
+//		        Circle preview = new Circle();
+//		        preview.setRadius(20);
+//		        preview.setFill(Color.ORANGERED);
+//		        
+//		        Text previewText = new Text("1");
+//		        previewText.setFill(Color.ANTIQUEWHITE);
+//		        
+//		        previewText.setTranslateX(2);
+//		        previewText.setTranslateY(2);
+//		        
+//		        previewGroup.getChildren().addAll(preview, previewText);
+//		        
+//		        SnapshotParameters sp = new SnapshotParameters();
+//		        sp.setFill(Color.TRANSPARENT);
+//		        
+//		        db.setDragView(previewGroup.snapshot(sp, null), event.getX(), event.getY());
+//		        
+//		        /* Put a string on a dragboard */
+//		        ClipboardContent content = new ClipboardContent();
+//		        content.putString(button.getText());
+//		        db.setContent(content);
+//		        
+//		        event.consume();
+//		    }
+//		});
+//
+//		button.setOnDragDone(new EventHandler<DragEvent>() {
+//			public void handle(DragEvent event) {
+////				System.out.println(event.get);
+//				System.out.println(button.getLayoutX());
+//				
+//				event.consume();
+//			}
+//		});
+	    
+	    
+	    
+	    //trial
+	    
+	    
+	    
+	    
+	    Text t = new Text("Trial");
+	    t.setOnDragDetected(e-> {
+	    	 Dragboard db = t.startDragAndDrop(TransferMode.ANY);
+	    	 
+	    	 ClipboardContent content = new ClipboardContent();
+	         content.putString(t.getText());
+	         db.setContent(content);
+	         
+	         e.consume();
+	    });
+	    
+	    
+	    //List of data
 		ListView<Text> list = new ListView<>();
 		list.setOrientation(Orientation.HORIZONTAL);
 		list.setMaxHeight(50);
+		list.getItems().add(t);
 		
 
 
