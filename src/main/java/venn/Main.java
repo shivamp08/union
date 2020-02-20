a2bbb13c6e959aea71743e5b9c262ebff320cd68
Shivam Patel <Shivam-patel@live.com>
2020-02-20 13:34:46 -0500

    CTRL + N tooltip

--- src/main/java/venn/Main.java
+++ src/main/java/venn/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.scene.input.*;
@@ -73,10 +74,12 @@ public class Main extends Application {
 		dragHbox.getChildren().add(VennPanelTitle.create("Items: ", false));
 		
 		//Add Button
+		Tooltip CtrlN = new Tooltip("CTRL + N"); 
 		Button add = new Button("Add Entry");
 		add.setOnAction(event -> VennAddEntry.add(this.entries));
 		add.setTranslateY(80);
 		add.setTranslateX(5);
+		Tooltip.install(add, CtrlN);
 		layout.getChildren().add(add);
 		
 		
