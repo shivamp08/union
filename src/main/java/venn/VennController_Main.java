8ff51c39590528ebcab0d741d1e32b2504bdb42f
rushabh-chauhan <rushabhh468@sgvp.in>
2020-03-07 16:58:29 -0500

    drag drop

--- src/main/java/venn/VennController_Main.java
+++ src/main/java/venn/VennController_Main.java
@@ -16,14 +16,19 @@ import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
+import javafx.scene.effect.Light.Point;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
 public class VennController_Main  implements Initializable{
@@ -37,21 +42,25 @@ public class VennController_Main  implements Initializable{
 
 	@FXML
 	private Path intersect;
-	
+
 	@FXML
-    private Group group;
-	
-	 
-	 @FXML
-	 private MenuItem Add;
-	 
-	  @FXML
-	  private MenuItem Colour;
-	  
-	  @FXML
-	  private JFXMasonryPane dataPane;
-	 
-	 
+	private Group group;
+
+
+	@FXML
+	private MenuItem Add;
+
+	@FXML
+	private MenuItem Colour;
+
+	@FXML
+	private JFXMasonryPane dataPane;
+
+
+
+
+
+
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb)
@@ -62,25 +71,25 @@ public class VennController_Main  implements Initializable{
 		intersect.getElements().addAll(path.getElements());
 		intersect.setLayoutX(0);
 		intersect.setLayoutY(0);
-		
-		
-		
+
+
+
 	}
-	
+
 	@FXML
-    void colourScene(ActionEvent event) throws Exception{
-		
+	void colourScene(ActionEvent event) throws Exception{
+
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(getClass().getResource("colourFXML.fxml"));
 		Parent colour = loader.load();		
-		
+
 		ColourCountroller controller = loader.getController();
-		
+
 		// binds the colour property to the circles
-			left.fillProperty().bind(controller.leftI.fillProperty());
-			right.fillProperty().bind(controller.rightI.fillProperty());
-			
-		
+		left.fillProperty().bind(controller.leftI.fillProperty());
+		right.fillProperty().bind(controller.rightI.fillProperty());
+
+
 		// Create the new scene
 		Stage stage = new Stage();
 		stage.initModality(Modality.APPLICATION_MODAL);
@@ -89,44 +98,87 @@ public class VennController_Main  implements Initializable{
 		stage.setResizable(false);
 		stage.getOnCloseRequest();
 		stage.showAndWait();
-		
-		
+
+
 	}
-	
-	
-	 @FXML
-	 void addData(ActionEvent event) {
-		 
-		 Text data = AddData.display();
-		 Label label = new Label();
-		 label.setText(data.getText());
-		 label.setOnDragDetected(e ->{
-			 Dragboard db = label.startDragAndDrop(TransferMode.MOVE);
-		        
-		        /* Put a string on a dragboard */
-		        ClipboardContent content = new ClipboardContent();
-		        content.putString(label.getText());
-		        db.setContent(content);
-		        
-		        event.consume();
-		 });
-		 dataPane.getChildren().add(label); 
-	 }
-	 
-	  @FXML
-	  void handleDragOver(DragEvent event) {
-		  if (event.getDragboard().hasString()) {
-	            /* allow for both copying and moving, whatever user chooses */
-	            event.acceptTransferModes(TransferMode.MOVE);
-	        }
-	        
-	        event.consume();
-	    }
-	  
-	  @FXML
-	  void handleDrop(DragEvent event) {
-		  
-	    }
+
+
+	@FXML
+	void addData(ActionEvent event) {
+
+		// Creates new label and give it texts.
+		Text data = AddData.display();
+		Label label = new Label();
+		label.setText(data.getText());
+		// drag dected method
+		label.setOnDragDetected(e ->{
+			Dragboard db = label.startDragAndDrop(TransferMode.MOVE);
+
+			/* Put a string on a dragboard */
+			ClipboardContent content = new ClipboardContent();
+			content.putString(label.getText());
+			db.setContent(content);
+
+			event.consume();
+		});
+
+		dataPane.getChildren().add(label); // adding the label to the data.
+
+		// drag done method on label
+		label.setOnDragDone(e -> {
+			//System.out.print(e);
+
+
+			this.dataPane.getChildren().remove(label);
+
+			e.consume();
+
+		});
+	}
+
+	// drag over method for the diagram.
+	@FXML
+	void handleDragOver(DragEvent event) {
+		if (event.getDragboard().hasString()) {
+			/* allow for both copying and moving, whatever user chooses */
+			event.acceptTransferModes(TransferMode.MOVE);
+		}
+
+		event.consume();
+	}
+
+	// drop handel method for the diagram.
+	@FXML
+	void handleDrop(DragEvent event) {
+
+		Dragboard d = event.getDragboard();
+
+		Pane pane = new Pane();
+
+		Text t = new Text(d.getString());
+		pane.getChildren().add(t);
+		this.group.getChildren().add(pane);
+
+
+
+		// setting the plane layouts to the cursor.
+		event.setDropCompleted(false);
+		this.group.setOnMouseMoved(e->{
+			if(!event.isDropCompleted())
+			{
+				pane.setLayoutX(e.getX());
+				pane.setLayoutY(e.getY());
+				event.setDropCompleted(true);
+			}
+			e.consume();
+		});
+
+
+
+		event.consume();
+	}
+
+
 
 
 	public Circle getLeft() {
@@ -139,5 +191,5 @@ public class VennController_Main  implements Initializable{
 }
 
 
-	
+
 
