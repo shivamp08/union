cf6cc6c7a852e89081f82156429d790c93b86f83
rushabh-chauhan <rushabhh468@sgvp.in>
2020-03-21 18:28:17 -0400

    new launch window

--- src/main/java/venn/VennStartUp.java
+++ src/main/java/venn/VennStartUp.java
@@ -1,40 +1,60 @@
 package venn;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 
+
 public class VennStartUp extends Application {
     //Has a menu that fits entire screen
+	
+	protected Stage stage;
+	
+	 @FXML
+	 private AnchorPane root;
+	
     @Override
-    public void start(Stage primaryStage) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/StartWindow.fxml"));
+    public void start(Stage primaryStage) throws IOException{
+    		
+    		this.stage = primaryStage;
+    		
+            Parent root = FXMLLoader.load(getClass().getResource("StartWindow.fxml"));
             Scene scene = new Scene(root);
-            primaryStage.setScene(scene);
+            stage.setScene(scene);
 
             //new view
-            primaryStage.initStyle(StageStyle.UNDECORATED);
+            stage.initStyle(StageStyle.UNDECORATED);
 
-            primaryStage.show();
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+            stage.show();
+       
     }
 
     public static void main(String[] args) {
         launch(args);
     }
+    
+    
 
     public void begin(ActionEvent actionEvent) {
+    	
+    	Main app = new Main();
+    	Stage stage = new Stage();
+    	app.start(stage);
+    	
+    	
     }
 
     public void close(MouseEvent mouseEvent) {
+    	root.getScene().getWindow().hide();
     }
 }
